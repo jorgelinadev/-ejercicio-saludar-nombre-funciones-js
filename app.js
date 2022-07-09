8f557cdf64fa4927f25f0c7dccbeecbda067f859
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-09 18:42:52 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//alert ('Mi archivo funciona');
+
+//saludar(nombre)
+//Crear una función saludar que tome como argumentos un nombre y devuelva un saludo que lo incluya.
+
+//saludar('Ada') // 'Hola Ada, un gusto conocerte'
+
+const ingresarNombre = ('nombre');
+
+const saludar = (nombre) => {
+    return `Hola ${(nombre)}, un gusto conocerte`;
+}
+
+console.log (saludar('nombre'));
+
+console.log(saludar('Jor'));
+console.log(saludar('Nacho'));
+console.log(saludar('Tincho'));
+console.log(saludar('Matute'));
